7815b5a1431d8d4710121ad1fed8bccd9ff7f10e
Paras Saini <126291053+Paras-17@users.noreply.github.com>
2024-10-18 17:41:28 +0000

    MongoDB integration

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+# MongoDB Configuration
+app.config["MONGO_URI"] = "mongodb://localhost:27017/learning_platform_db"
+mongo = PyMongo(app)
+
 @app.route('/')
 def home():
-    return "Welcome to the Learning Platform"
+    return "MongoDB Connected!"
 
 if __name__ == '__main__':
     app.run(debug=True)
